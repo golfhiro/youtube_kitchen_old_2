6a437f100f5b59825b91fccca1eeb067836414a1
golfhiro <hiroki5118@gmail.com>
2023-03-02 15:57:35 +0900

    動画の取得と表示の実装（仮）

--- api/app/controllers/tops_controller.rb
+++ api/app/controllers/tops_controller.rb
@@ -1,4 +1,12 @@
 class TopsController < ApplicationController
+  require 'google/apis/youtube_v3'
+
   def index
+    youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    youtube.key = Rails.application.credentials.youtube_api[:youtube_api_key]
+
+    @videos = Rails.cache.fetch('search_result_list', expires_in: 12.hours) do
+      youtube.list_searches("id,snippet", q: "GReeeeN", type: "video", max_results: 1)
+    end || []
   end
 end
