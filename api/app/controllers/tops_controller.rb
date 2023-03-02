88fc31c2763d3f0a54044837f56bad056e931b10
golfhiro <hiroki5118@gmail.com>
2023-03-02 21:03:24 +0900

    検索ワードを投げて動画を取得&表示(途中)

--- api/app/controllers/tops_controller.rb
+++ api/app/controllers/tops_controller.rb
@@ -2,14 +2,29 @@ class TopsController < ApplicationController
   require 'google/apis/youtube_v3'
 
   def index
+  #   genres = ["和食", "洋食", "中華"]
+  #   ingredients =  ["豚肉", "牛肉", "鶏肉"]
+  #   youtubers = ["武島たけしの極み飯", "料理研究家リュウジのバズレシピ", "Koh Kentetsu Kitchen【料理研究家コウケンテツ公式チャンネル】", "だれウマ【料理研究家】", "鳥羽周作のシズるチャンネル ", "はるあん"]
 
-    #Youtube動画の取得
-    youtube = Google::Apis::YoutubeV3::YouTubeService.new
-    youtube.key = Rails.application.credentials.youtube_api[:youtube_api_key]
+  #   genre = genres.sample
+  #   ingredient = ingredients.sample
+  #   youtuber = youtubers.sample
 
-    @videos = Rails.cache.fetch('search_result_list', expires_in: 12.hours) do
-      youtube.list_searches("id,snippet", q: "GReeeeN", type: "video", max_results: 1)
-    end || []
+  #   # キャッシュがあればそれを返し、なければ新しく取得してキャッシュに保存
+  #   @videos = Rails.cache.fetch('search_result_list', expires_in: 1.hour) do
+  #     youtube = Google::Apis::YoutubeV3::YouTubeService.new
+  #     youtube.key = Rails.application.credentials.youtube_api[:youtube_api_key]
 
+  #     youtube.list_searches(
+  #       "id,snippet",
+  #       q: "#{genre} #{ingredient} #{youtuber} レシピ",
+  #       type: "video",
+  #       video_definition: "high",
+  #       video_duration: "long",
+  #       order: "relevance",
+  #       max_results: 1,
+  #     )
+  #   end
   end
+
 end
