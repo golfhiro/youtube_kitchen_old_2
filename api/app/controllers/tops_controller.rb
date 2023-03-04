da86168a7c66faed59c89358643128c87c834b25
golfhiro <hiroki5118@gmail.com>
2023-03-04 20:08:29 +0900

    曜日ごとに材料とジャンルが違う献立を作成する処理を追加

--- api/app/controllers/tops_controller.rb
+++ api/app/controllers/tops_controller.rb
@@ -37,51 +37,43 @@
 #   end
 # end
 
-###　材料が違う7つの動画を曜日ごとに表示。
+
 class TopsController < ApplicationController
   require 'google/apis/youtube_v3'
 
   def index
-    #材料を用意
-    ingredients = ["豚肉", "鶏肉", "鯖,", "牛肉", "鮭", "豆腐", "キャベツ", "挽肉", "海老", "白菜", "まぐろ"]
-    select_ingredients = ingredients.sample(7)
+    # キャッシュに保存された結果があればそれを返す
+    @menu = Rails.cache.fetch('menu', expires_in: 12.hours) do
+      # キャッシュに保存された結果がない場合、新しく検索を行う
+      # 材料のキーワードを用意
+      ingredients = ["豚肉", "鶏肉", "鯖,", "牛肉", "鮭", "豆腐", "キャベツ", "挽肉", "海老", "白菜", "まぐろ"]
+      # ジャンルのキーワードを用意
+      genres = ["和食", "洋食", "中華"]
 
-    @related_videos = []
-    @menu = {}
+      # 1週間分の献立を用意
+      menu = {}
+      %w[ Monday Tuesday Wednesday Thursday Friday Saturday Sunday].each do |day|
+        # 材料とジャンルをランダムに1つずつ選択
+        ingredient = ingredients.sample
+        genre = genres.sample
 
-    #キャッシュにあればそれを返し、なければ更新
-    @videos = Rails.cache.fetch('search_result_list', expires_in: 12.hour) do
-      youtube = Google::Apis::YoutubeV3::YouTubeService.new
-      youtube.key = Rails.application.credentials.youtube_api[:youtube_api_key]
-
-      select_ingredients.each do |ingredient|
-        responce = youtube.list_searches(
+        # YouTube APIを使用して動画を検索
+        youtube = Google::Apis::YoutubeV3::YouTubeService.new
+        youtube.key = Rails.application.credentials.youtube_api[:youtube_api_key]
+        response = youtube.list_searches(
           'snippet',
-          q: ingredient + "レシピ",
+          q: ingredient + "レシピ" + genre,
           type: 'video',
           order: "relevance",
           max_results: 1
         )
 
-        responce.items.each do |item|
-          @related_videos << {
-            # title: item.snippet.title,
-            video_id: item.id.video_id,
-            # thumbnail: item.snippet.thumbnails.high.url
-          }
-        end
+        # 検索結果を1週間分の献立に追加
+        menu[day] = [{ video_id: response.items.first.id.video_id }]
       end
 
-        # 曜日ごとに動画をグループ化する
-        days = %w[ Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
-        @related_videos.each_with_index do |video, index|
-          day_index = index % 7
-          day = days[day_index]
-          @menu[day] ||= []
-          @menu[day] << video
-        end
-
-      return @menu
+      # 検索結果をキャッシュに保存する
+      menu
     end
   end
 end
